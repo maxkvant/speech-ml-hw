011cf0209454b3e7237630cf43283d47ac074bc5
maxkvant <max.kvant@gmail.com>
2019-01-29 21:06:17 +0300

    added yaml config

--- 1/add_noise.py
+++ 1/add_noise.py
@@ -1,55 +1,68 @@
 import numpy as np
-import random
 import librosa
 import matplotlib.pyplot as plt
 import argparse
-import soundfile as sf
-
+import os
+import random
+import yaml
 
 eps = 1e-9
 samplerate = 16000
 
 
 class NoiseAdder:
-    def __init__(self, fragment, ratio=0.1, normalize=True, random_start=True):
-        assert len(fragment) > 1
-        self.fragment = fragment.copy()
-        self.normalize = normalize
-        self.ratio = ratio
+    def __init__(self, fragment_generators, random_start=True):
+        self.fragment_generators = fragment_generators
         self.random_start = random_start
 
     def add_noise(self, sound):
         n = len(sound)
         assert n > 1
 
-        rand_shift = random.randint(0, len(self.fragment))
-        fragment = np.roll(self.fragment, rand_shift if self.random_start else 0)
+        noise = np.zeros(n)
+        for fragment_gen in self.fragment_generators:
+            fragment = fragment_gen()
+            rand_shift = random.randint(0, len(fragment))
+            fragment = np.roll(fragment, rand_shift if self.random_start else 0)
 
-        noise = np.tile(fragment, n // len(fragment) + 1)[:n]
-        if self.normalize:
-            noise = noise * np.std(sound) / np.std(noise)
-
-        return sound + self.ratio * noise
+            noise += np.tile(fragment, n // len(fragment) + 1)[:n]
+        return sound + noise
 
 
 def load_audio(file_path):
     data, _ = librosa.core.load(file_path, samplerate)
     return data
 
-def plot_time_series(data):
-    fig = plt.figure(figsize=(14, 8))
-    plt.title('Raw wave ')
-    plt.ylabel('Amplitude')
-    plt.plot(np.linspace(0, 1, len(data)), data)
-    plt.show()
 
+def audio_getter(directory, ratio):
+    filenames = [directory + os.sep + filename for filename in os.listdir(directory)]
+    filenames = [filename for filename in filenames
+                 if os.path.isfile(filename) and (filename.endswith(".wav") or filename.endswith(".flac"))]
+
+    assert len(filenames) > 0
+    def gen_audio():
+        filename = random.choice(filenames)
+        return ratio * load_audio(filename)
+    return gen_audio
+
+
+def create_noise_adder(noise_yaml):
+    fragment_generators = []
+    for item in noise_yaml["noise_info"]:
+        path = item["path"]
+        ratio = float(item["ratio"])
+        if os.path.isfile(path) and os.path.exists(path):
+            fragment_generators.append(lambda: load_audio(path) * ratio)
+        elif os.path.isdir(path) and os.path.exists(path):
+            fragment_generators.append(audio_getter(path, ratio))
+        else:
+            raise("no file or directory " + path)
+    return NoiseAdder(fragment_generators)
 
 def createparser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input', help="Input file (.wav or .flac)", default='speech.wav')
-    parser.add_argument('-n', '--noise', help="Noise file (.wav or .flac)", default='office01.wav')
-    parser.add_argument('-r', '--ratio', help="Noise ratio", default=0.5)
-    parser.add_argument('-a', '--normalize', help="Normalize", default=True)
+    parser.add_argument('-i', '--input', help="Input file (.wav or .flac)")
+    parser.add_argument('-n', '--noise', help="Noise discription (.yaml)", default='noise.yaml')
     parser.add_argument('-o', '--output', help="Output file (.wav or .flac)", default='output.wav')
     return parser
 
@@ -57,21 +70,17 @@ def createparser():
 def main():
     parser = createparser()
     args = parser.parse_args()
+
     input_file = args.input
-    noise_file = args.noise
-    ratio = args.ratio
-    normalize = args.normalize
     output_file = args.output
+    with open(args.noise, 'r') as f:
+        noise_yaml = yaml.load(f)
 
     sound = load_audio(input_file)
-    noise = load_audio(noise_file)
-
-    sound = np.asarray(sound)
-    noise = np.asarray(noise)
-
-    output = NoiseAdder(noise, ratio=ratio, normalize=normalize).add_noise(sound)
+    output = create_noise_adder(noise_yaml).add_noise(sound)
 
     librosa.output.write_wav(output_file, output, samplerate)
+    os.system("cvlc " + output_file)
 
 
 if __name__ == '__main__':
