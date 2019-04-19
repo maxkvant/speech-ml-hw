181e369a687581c4119e979e7ed7a8b7e35be7e8
maxkvant <max.kvant@gmail.com>
2019-04-19 04:15:51 +0300

    added feature extraction

--- 2/homework/laughter_prediction/feature_extractors.py
+++ 2/homework/laughter_prediction/feature_extractors.py
@@ -1,8 +1,9 @@
 import os
 import tempfile
+import librosa
 
 import pandas as pd
-import laughter_classification.psf_features as psf_features
+import numpy as np
 
 
 class FeatureExtractor:
@@ -30,4 +31,37 @@ class PyAAExtractor(FeatureExtractor):
             os.system("source activate {}; {}".format(self.py_env_name, cmd))
 
             feature_df = pd.read_csv(feature_save_path)
+        return feature_df
+
+
+class LibrosaExtractor(FeatureExtractor):
+    """Python Audio Analysis features extractor"""
+    def __init__(self, window_size=0.01):
+        self.window_size = window_size
+
+    def extract_features(self, wav_path):
+        y, sr = librosa.load(wav_path)
+
+        frame_len = int(sr * self.window_size)
+        mel = []
+        mfcc = []
+
+        for frame_start in range(0, len(y) - frame_len, frame_len):
+            frame = y[frame_start: frame_start + frame_len]
+
+            mel_feature = librosa.feature.melspectrogram(frame, sr)
+            mel_feature = librosa.power_to_db(mel_feature)
+            mel.append(np.average(mel_feature, axis=1))
+
+            mfcc_feature = librosa.feature.mfcc(frame, sr)
+            mfcc.append(np.average(mfcc_feature, axis=1))
+
+        mel = np.asarray(mel)
+        mfcc = np.asarray(mfcc)
+
+        mel_df  = pd.DataFrame(mel,  columns=[ "mel_{0:03d}".format(i) for i in range( mel.shape[1])])
+        mfcc_df = pd.DataFrame(mfcc, columns=["mfcc_{0:03d}".format(i) for i in range(mfcc.shape[1])])
+
+        feature_df = pd.concat([mel_df, mfcc_df], axis=1)
+
         return feature_df
